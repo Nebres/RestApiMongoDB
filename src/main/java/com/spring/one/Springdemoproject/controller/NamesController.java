4016b6bc5ccc509519b8f45cc35bf416a3927fc5
Nebres <serbenski@o2.pl>
2019-03-24 23:59:59 +0100

    fixing controller

--- src/main/java/com/spring/one/Springdemoproject/controller/NamesController.java
+++ src/main/java/com/spring/one/Springdemoproject/controller/NamesController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
-@EnableWebMvc
 public class NamesController {
 
     @Autowired
